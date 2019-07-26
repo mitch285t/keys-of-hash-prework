577477ab31b046e2f3584cdd0f3608816683b771
mitchell williams <mscottwilliams1994@gmail.com>
2019-07-26 19:53:15 +0000

    Automatically backed up by Learn

--- lib/keys_of_hash.rb
+++ lib/keys_of_hash.rb
@@ -4,7 +4,16 @@ class Hash
   
   def keys_of(arguments, *hash)
    
+       array_of_keys = []
+    puts hash.class
+    self.each do |key, value|
+      # puts "Key is #{key}. Value is #{value}"
+      if value == arguments || value == hash[0]
+        array_of_keys << key
+      end
+    end
+    array_of_keys
+  end	  end
+end 	end 
      
-     
-   end 
- end
+   
